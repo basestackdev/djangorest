1628713c16f8d2e712f287d3b030a55b0f0b0df4
Fikri Amin <mohdfikrimohdamin@gmail.com>
2020-06-17 16:35:13 +0800

    add 1st commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/fikriamin/work/python/pylearn/tdd/django-tdd-docker/app/env/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ movies/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# app/movies/models.py
+
+from django.contrib.auth.models import AbstractUser
+class CustomUser(AbstractUser):
+    pass
